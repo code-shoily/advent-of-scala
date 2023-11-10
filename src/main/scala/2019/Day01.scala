f6b4013a0b269153e0c9e495ba39642542e51224
Mafinar K <mafinar@gmail.com>
2023-11-09 21:39:33 -0500

    Solve 2019/1

---
+++ src/main/scala/2019/Day01.scala
@@ -0,0 +1,44 @@
+/** Day 1: Sonar Sweep
+  *
+  * Link: https://adventofcode.com/2019/day/1
+  *
+  * Difficulty: xs
+  *
+  * Tags: recursion reduction
+  */
+package year_2019
+
+import base.Solution
+
+type InputType = Seq[Int]
+
+class Day01(rawInput: List[String]):
+  def solvePart1(input: InputType): Int =
+    input.map(_ / 3 - 2).reduce(_ + _)
+
+  def solvePart2(input: InputType) =
+    def getFuel(mass: Int, fuel: Int = 0): Int =
+      (mass / 3 - 2) match
+        case notFuel if notFuel <= 0 => fuel
+        case fuelPart                => getFuel(fuelPart, fuelPart + fuel)
+
+    input.map(getFuel(_)).reduce(_ + _)
+
+  def solve(): Solution =
+    val input = parseInput
+    val part1 = solvePart1(input)
+    val part2 = solvePart2(input)
+    (part1, part2)
+
+  def parseInput: Seq[Int] = rawInput.map(_.toInt)
+
+/*--------- Block to test this file on IDEs, comment this line with `//` to enable.
+@main def run_2019_01 =
+  import utils.io.{readLines, printSolution}
+
+  readLines(2019, 1) match
+    case Some(rawInput) =>
+      val solver = Day01(rawInput)
+      printSolution(solver.solve())
+    case _ => sys.error("Could not read file")
+// */
--- src/main/scala/2019/Solver.scala
+++ src/main/scala/2019/Solver.scala
@@ -5,4 +5,5 @@ import base.*
 class Solver2019(input: List[String]) extends Solver:
   override def solveFor(day: Int): Solution =
     day match
+      case 1 => Day01(input).solve()
       case _ => sys.error(s"Did not solve day 2019/$day yet")
