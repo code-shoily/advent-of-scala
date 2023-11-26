cb85204aa96982dd29511b5d18e74458b6daeac4
Mafinar K <mafinar@gmail.com>
2023-11-26 07:07:47 -0500

    Solve 2017/22

---
+++ src/main/scala/advent_of_scala/2017/Day22.scala
@@ -0,0 +1,87 @@
+/** 2017/22: Sporifica Virus
+  *
+  * Link: https://adventofcode.com/2017/day/22
+  *
+  * Difficulty: s
+  *
+  * Tags: game-of-life slow rotation
+  *
+  * Answers: (5447, 2_511_705)
+  */
+package advent_of_scala.year_2017
+
+import advent_of_scala.base.{Solution, impossibleStateError}
+import Day22.*
+import Node.*
+
+type InputType22 = State
+
+class Day22(rawInput: List[String]):
+    def solvePart1(input: InputType22): Int =
+        def step(state: State): State = state.grid(state.location) match
+            case Clean    => state.next(Infected, state.direction.ccw, 1)
+            case Infected => state.next(Clean, state.direction.cw, 0)
+            case _        => impossibleStateError
+
+        Iterator.iterate(input)(step).drop(10_000).next().infected
+    end solvePart1
+
+    def solvePart2(input: InputType22): Int =
+        def step(state: State): State = state.grid(state.location) match
+            case Clean    => state.next(Weakened, state.direction.ccw, 0)
+            case Weakened => state.next(Infected, state.direction, 1)
+            case Infected => state.next(Flagged, state.direction.cw, 0)
+            case Flagged  => state.next(Clean, state.direction.cw.cw, 0)
+
+        Iterator.iterate(input)(step).drop(10_000_000).next().infected
+    end solvePart2
+
+    def solve: Solution =
+        val input = parsedInput
+        val part1 = solvePart1(input)
+        val part2 = solvePart2(input)
+        (part1, part2)
+    end solve
+
+    private def parsedInput: InputType22 =
+        val points =
+            for
+                y <- rawInput.indices
+                x <- rawInput.indices
+                if rawInput(y)(x) == '#'
+            yield Point(x, y)
+        val start =
+            points
+                .map(_ -> Infected)
+                .toMap
+                .withDefaultValue(Clean)
+
+        State(start, Point(rawInput.size / 2, rawInput.size / 2), Point(0, -1), 0)
+    end parsedInput
+end Day22
+
+object Day22:
+    case class Point(x: Int, y: Int):
+        def +(other: Point): Point = Point(x + other.x, y + other.y)
+        def cw: Point = Point(-y, x)
+        def ccw: Point = Point(y, -x)
+    end Point
+
+    enum Node:
+        case Clean, Weakened, Infected, Flagged
+
+    case class State(grid: Map[Point, Node], location: Point, direction: Point, infected: Int):
+        def next(node: Node, direction: Point, infect: Int): State =
+            State(grid.updated(location, node), location + direction, direction, infected + infect)
+end Day22
+
+/*--------- Block to test this file on IDEs, comment this line with `//` to enable.
+@main def run_2017_22 =
+    import advent_of_scala.utils.IO.{readLines, printSolution}
+    import advent_of_scala.base.impossibleStateError
+    readLines(2017, 22) match
+        case Some(raw_input) =>
+            printSolution(Day22(raw_input).solve)
+        case _ => impossibleStateError
+end run_2017_22
+// */
---
+++ src/test/scala/advent_of_scala/2017/Day22Suite.scala
@@ -0,0 +1,17 @@
+package advent_of_scala.year_2017
+
+import advent_of_scala.utils.IO.readLines
+import advent_of_scala.year_2017.Day22
+
+class Day22Suite extends munit.FunSuite:
+    val year = 2017
+    val day = 22
+    val result = (5447, 2_511_705)
+
+    test("Day 22 solve yields correct result") {
+        val rawInput = readLines(year, day).get
+        val solution = Day22(rawInput).solve
+
+        assertEquals(solution, result)
+    }
+end Day22Suite
